d142cf7c9b176b13267209f5723ce8ed4564b813
Kirill <kirilldeals7@gmail.com>
2023-07-28 20:19:31 +0700

    Improve Video Delete page. Some changes in upload video js file

--- Videlo/wwwroot/js/videomanagement/ui/upload-video-submit.js
+++ Videlo/wwwroot/js/videomanagement/ui/upload-video-submit.js
@@ -21,12 +21,14 @@ $(document).ready(function () {
                 const status = successResponse.xhr.status;
                 console.log(status);
                 if (status === 200) {
-                    location.reload();
+                    window.location.href = '/Channel/Studio';
                 }
             } catch (error) {
                 if (error.status === 503) {
-                    alert(`You already have a video in the uploading process. 
-                        Please wait until the current upload is finished.`)
+                    alert(`You already have a video in the uploading process. Please wait until the current upload is finished.`);
+
+                    $(awaitContainerId).empty();
+                    $(submitBtnId).prop('disabled', false);
                 }
             }
         }
